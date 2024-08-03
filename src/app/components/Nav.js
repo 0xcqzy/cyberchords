25d9284099b31f8cee2a61242351dcab69d4cdd4
jayantibalmuchu <jayanti98b@gmail.com>
2024-08-03 19:37:27 +0530

    change Nav UI

--- src/app/components/Nav.js
+++ src/app/components/Nav.js
@@ -1,16 +1,50 @@
+"use client";
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import styles from './Nav.module.css';
 
 const Nav = () => {
+  const pathname = usePathname(); // Get the current pathname
+
+  // Function to check if the link is active
+  const isActive = (href) => pathname === href;
+
   return (
     <nav className={styles.navbar}>
       <div className={styles.left}>
-        <Link className={styles.link} href="/">Art</Link>
-        <Link className={styles.link} href="/commercial">Commercial</Link>
+        <Link className={`${styles.col} ${styles.link}`} href="/">
+          <div>art</div>
+          <div
+            id={styles.box1}
+            className={styles.box}
+            style={{ opacity: isActive('/') ? 1 : '' }}
+          ></div>
+        </Link>
+
+        <Link className={`${styles.col} ${styles.link}`} href="/commercial">
+          <div>commercial</div>
+          <div
+            id={styles.box2}
+            className={styles.box}
+            style={{ opacity: isActive('/commercial') ? 1 : '' }}
+          ></div>
+        </Link>
       </div>
+
       <div className={styles.right}>
-        <Link className={styles.link} href="/concept-studio">Concept Studio</Link>
-        <Link className={styles.link} href="/info">Info</Link>
+        <Link className={`${styles.col} ${styles.link}`} href="/concept-studio">
+          <div
+            id={styles.box3} className={styles.box} style={{ opacity: isActive('/concept-studio') ? 1 : ''}}
+          ></div>
+          <div>concept studio</div>
+        </Link>
+
+        <Link className={`${styles.col} ${styles.link}`} href="/info">
+          <div
+            id={styles.box4} className={styles.box} style={{ opacity: isActive('/info') ? 1 : ''}}
+          ></div>
+          <div>info</div>
+        </Link>
       </div>
     </nav>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,28 @@
-import Nav from "./components/Nav";
+"use client";
+import { useState, useEffect } from 'react';
+import styles from './page.module.css'
+import Nav from './components/Nav';
+import Image from 'next/image';
 
-const Home = () =>{
+
+
+const Home = () => {
+
     return (
-        <Nav></Nav>
+        <>
+            <Nav></Nav>
+            <div className={styles.imageWrapper}>
+                <Image
+                    src="/1.jpg"
+                    layout="fill"
+                    className={styles.image} // Apply the CSS class for the image
+                    alt="Description of the image" // Always provide alt text for accessibility
+                />
+            </div>
+
+
+        </>
+
     )
 }
 
