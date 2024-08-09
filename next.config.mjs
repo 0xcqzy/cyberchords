aa1af0703ef39ec191b3293f0b11592ae20e2fbc
jayantibalmuchu <jayanti98b@gmail.com>
2024-08-09 16:19:22 +0530

    Revert " Configure basePath for local and production environments"
    This reverts commit f1f07c8bf85934fca1e5c935eb000fb7e3d85f9e.

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   output: 'export',
   images: {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'ik.imagekit.io',
+        hostname: 'ik.imagekit.io', 
       },
     ],
   },
