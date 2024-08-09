818c573d662da2720f8f2f077d8b817c0242bc99
jayantibalmuchu <jayanti98b@gmail.com>
2024-08-09 21:58:20 +0530

    imagekit url simplification

--- next.config.mjs
+++ next.config.mjs
@@ -3,12 +3,7 @@ const nextConfig = {
   basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
   output: 'export',
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**', 
-      },
-    ],
+    unoptimized: true,
   },
 };
 
--- src/app/page.js
+++ src/app/page.js
@@ -5,25 +5,12 @@ import Nav from './components/Nav';
 import gsap from 'gsap';
 import Image from 'next/image';
 
-// Custom ImageKit loader function
-const imageKitLoader = ({ src, width, quality }) => {
-  if (src[0] === "/") src = src.slice(1);
-  const params = [`w-${width}`];
-  if (quality) {
-    params.push(`q-${quality}`);
-  }
-  const paramsString = params.join(",");
-  var urlEndpoint = " https://ik.imagekit.io/0xcqzy/"
-  if (urlEndpoint[urlEndpoint.length - 1] === "/") urlEndpoint = urlEndpoint.substring(0, urlEndpoint.length - 1);
-  return `${urlEndpoint}/${src}?tr=${paramsString}`;
-};
-
-// Array of image paths (not full URLs)
 const imageUrls = [
-  "Cyberchords/3.jpg",
-  "Cyberchords/2.jpg"
-  // Add more image paths here as needed
+  "https://ik.imagekit.io/0xcqzy/Cyberchords/3.jpg",
+  "https://ik.imagekit.io/0xcqzy/Cyberchords/2.jpg"
+  // Add more full URLs here as needed
 ];
+
 
 
 const Home = () => {
@@ -46,17 +33,16 @@ const Home = () => {
       <div ref={aboutRef} className={styles.about}>
         Cyberchords is all about that electric connection between the digital world and nature, blending a techy, futuristic feel with raw, natural beauty. Their art feels like a glitchy dream, where sleek sophistication meets a wild, unpolished edge. With a style that feels like a digital meditation, they&rsquo;re in sync, exploring the spaces where reality blurs into a digital daydream. Cyberchords pulls you into a world that&rsquo;s both real and surreal. It&rsquo;s a call to live a life full of tech love and natural respect.
       </div>
-
+      
       <div className={styles.imageWrapperContainer}>
         {imageUrls.map((url, index) => (
           <div key={index} className={styles.imageWrapper}>
             <Image
-            loader={imageKitLoader}
-            src={url}
-            alt={`Image ${index + 1}`}
-            layout="fill" // Fills the container while maintaining aspect ratio
-            objectFit="contain" // Ensures the image covers the container
-          />
+              src={url}
+              alt={`Image ${index + 1}`}
+              layout="fill" // Fills the container while maintaining aspect ratio
+              objectFit="contain" // Ensures the image covers the container
+            />
           </div>
         ))}
       </div>
