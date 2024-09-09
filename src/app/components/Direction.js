96fce78f77518241d7059c55ca80337c4f682d81
jayantibalmuchu <jayanti98b@gmail.com>
2024-09-10 01:37:37 +0530

    add content to direction section

--- src/app/components/Direction.js
+++ src/app/components/Direction.js
@@ -1,9 +1,27 @@
+import style from "./Direction.module.css";
+import React from 'react';
+import ReactPlayer from 'react-player';
+
 const Direction = () => {
-    return ( 
-    <>
-    <h1>Hello world</h1>
-    </>
+    return (
+        <>
+            <div className="style.main">
+                <div className={style.secondaryText}>At cyberchords Studio, we dance between the worlds of art and tech, weaving dreams into reality. Through fluid collaboration with the unseen, we craft soulful experiences for brands, blending creativity with cutting-edge technology. From the whispers of neural networks to the magic of virtual spaces, we harness the power of innovation in its purest form. Our journey flows through the realms of art, science, and the human spirit, all while embracing the ethereal energy of growth and expansion.</div>
+            </div>
+
+
+            <div className={style.videoWrapper}>
+                <ReactPlayer
+                    url="https://ik.imagekit.io/0xcqzy/cyberchords/5639717-uhd_4096_2160_25fps.mp4?updatedAt=1725911527016"
+                    playing
+                    muted
+                    loop
+                    width="100%"
+                    height="100%" // Make ReactPlayer take up 100% of the wrapper's height
+                />
+            </div>
+        </>
     )
 }
 
-export default Direction ; 
+export default Direction; 
